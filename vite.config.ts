bcaf5b763c7df82818fe80af27ad8c1c5a356e35
basic-user-iom <basic-user-iom@users.noreply.github.com>
2026-06-22 21:05:39 +0200

    Inject VIEWER_APP_VERSION into index.html at build time.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VIEWER_APP_VERSION } from './src/viewerAppMeta'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'inject-viewer-app-version',
+      transformIndexHtml(html) {
+        return html.replaceAll('%VIEWER_APP_VERSION%', VIEWER_APP_VERSION)
+      },
+    },
+  ],
   build: {
     rollupOptions: {
       input: ['index.html', 'orb-preview.html'],
